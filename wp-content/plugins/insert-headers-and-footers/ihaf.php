cfea2ada092fdbe3cb990b2e280f6be478767d56
Chris Hebron <christopher.hebron@businessol.com>
2020-01-16 17:58:35 +0000

    update - plugins - akistmet anti-spam, media replace, insert headers and footers, regenerate thumbnails and responsive lightbox & gallery

--- wp-content/plugins/insert-headers-and-footers/ihaf.php
+++ wp-content/plugins/insert-headers-and-footers/ihaf.php
@@ -2,14 +2,16 @@
 /**
 * Plugin Name: Insert Headers and Footers
 * Plugin URI: http://www.wpbeginner.com/
-* Version: 1.4.3
+* Version: 1.4.4
 * Author: WPBeginner
 * Author URI: http://www.wpbeginner.com/
 * Description: Allows you to insert code or text in the header or footer of your WordPress blog
 * License: GPL2
+* Text Domain: insert-headers-and-footers
+* Domain Path: languages
 */
 
-/*  Copyright 2016 WPBeginner
+/*  Copyright 2019 WPBeginner
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License, version 2, as
@@ -38,7 +40,7 @@ class InsertHeadersAndFooters {
         $this->plugin               = new stdClass;
         $this->plugin->name         = 'insert-headers-and-footers'; // Plugin Folder
         $this->plugin->displayName  = 'Insert Headers and Footers'; // Plugin Name
-        $this->plugin->version      = '1.4.2';
+        $this->plugin->version      = '1.4.4';
         $this->plugin->folder       = plugin_dir_path( __FILE__ );
         $this->plugin->url          = plugin_dir_url( __FILE__ );
         $this->plugin->db_welcome_dismissed_key = $this->plugin->name . '_welcome_dismissed_key';
@@ -51,47 +53,13 @@ class InsertHeadersAndFooters {
 		// Hooks
 		add_action( 'admin_init', array( &$this, 'registerSettings' ) );
         add_action( 'admin_menu', array( &$this, 'adminPanelsAndMetaBoxes' ) );
-        add_action( 'wp_feed_options', array( &$this, 'dashBoardRss' ), 10, 2 );
         add_action( 'admin_notices', array( &$this, 'dashboardNotices' ) );
         add_action( 'wp_ajax_' . $this->plugin->name . '_dismiss_dashboard_notices', array( &$this, 'dismissDashboardNotices' ) );
 
         // Frontend Hooks
         add_action( 'wp_head', array( &$this, 'frontendHeader' ) );
 		add_action( 'wp_footer', array( &$this, 'frontendFooter' ) );
-
-		// Filters
-		add_filter( 'dashboard_secondary_items', array( &$this, 'dashboardSecondaryItems' ) );
 	}
-
-    /**
-     * Number of Secondary feed items to show
-     */
-	function dashboardSecondaryItems() {
-		return 6;
-	}
-
-    /**
-     * Update the planet feed to add the WPB feed
-     */
-    function dashboardRss( $feed, $url ) {
-        // Return early if not on the right page.
-        global $pagenow;
-        if ( 'admin-ajax.php' !== $pagenow ) {
-            return;
-        }
-
-        // Return early if not on the right feed.
-        if ( strpos( $url, 'planet.wordpress.org' ) === false ) {
-            return;
-        }
-
-        // Only move forward if this action hasn't been done already.
-        if ( ! $GLOBALS['wpb_feed_append'] ) {
-            $GLOBALS['wpb_feed_append'] = true;
-            $urls = array( 'http://www.wpbeginner.com/feed/', $url );
-            $feed->set_feed_url( $urls );
-        }
-    }
 
     /**
      * Show relevant notices for the plugin
@@ -140,7 +108,7 @@ class InsertHeadersAndFooters {
     function adminPanel() {
 		// only admin user can access this page
 		if ( !current_user_can( 'administrator' ) ) {
-			echo '<p>' . __( 'Sorry, you are not allowed to access this page.', $this->plugin->name ) . '</p>';
+			echo '<p>' . __( 'Sorry, you are not allowed to access this page.', 'insert-headers-and-footers' ) . '</p>';
 			return;
 		}
 
@@ -149,10 +117,10 @@ class InsertHeadersAndFooters {
         	// Check nonce
 			if ( !isset( $_REQUEST[$this->plugin->name.'_nonce'] ) ) {
 	        	// Missing nonce
-	        	$this->errorMessage = __( 'nonce field is missing. Settings NOT saved.', $this->plugin->name );
+	        	$this->errorMessage = __( 'nonce field is missing. Settings NOT saved.', 'insert-headers-and-footers' );
         	} elseif ( !wp_verify_nonce( $_REQUEST[$this->plugin->name.'_nonce'], $this->plugin->name ) ) {
 	        	// Invalid nonce
-	        	$this->errorMessage = __( 'Invalid nonce specified. Settings NOT saved.', $this->plugin->name );
+	        	$this->errorMessage = __( 'Invalid nonce specified. Settings NOT saved.', 'insert-headers-and-footers' );
         	} else {
 	        	// Save
 				// $_REQUEST has already been slashed by wp_magic_quotes in wp-settings
@@ -160,7 +128,7 @@ class InsertHeadersAndFooters {
 	    		update_option( 'ihaf_insert_header', $_REQUEST['ihaf_insert_header'] );
 	    		update_option( 'ihaf_insert_footer', $_REQUEST['ihaf_insert_footer'] );
 	    		update_option( $this->plugin->db_welcome_dismissed_key, 1 );
-				$this->message = __( 'Settings Saved.', $this->plugin->name );
+				$this->message = __( 'Settings Saved.', 'insert-headers-and-footers' );
 			}
         }
 
@@ -171,14 +139,14 @@ class InsertHeadersAndFooters {
         );
 
     	// Load Settings Form
-        include_once( WP_PLUGIN_DIR . '/' . $this->plugin->name . '/views/settings.php' );
+        include_once( $this->plugin->folder . '/views/settings.php' );
     }
 
     /**
 	* Loads plugin textdomain
 	*/
 	function loadLanguageFiles() {
-		load_plugin_textdomain( $this->plugin->name, false, $this->plugin->name . '/languages/' );
+		load_plugin_textdomain( 'insert-headers-and-footers', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 	}
 
 	/**
