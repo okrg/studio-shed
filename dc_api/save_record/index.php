b382f4665543b7ad650b939db5693570b8a1a99a
Rolo Garcia <rolando.garcia@businessol.com>
2020-02-21 22:01:58 +0000

    Fix for header detection of x-api-key

--- dc_api/save_record/index.php
+++ dc_api/save_record/index.php
@@ -1,8 +1,18 @@
 <?php
 header('Content-Type: application/json');
 header('Cache-Control: no-cache, must-revalidate, max-age=0');
+if (!function_exists('getallheaders')) {
+  function getallheaders() {
+    $headers = [];
+    foreach ($_SERVER as $name => $value) {
+      if (substr($name, 0, 5) == 'HTTP_') {
+        $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+      }
+    }
+    return $headers;
+  }
+}
 $headers = getallheaders();
-
 if (isset($headers['x-api-key'])) {
   if ($headers['x-api-key'] !== '1234567890') {
     exit(json_encode(['error' => 'Wrong API key.']));
