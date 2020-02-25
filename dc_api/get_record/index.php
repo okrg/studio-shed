ad523aeb39e397330bbcac848677e950db7769eb
Rolo Garcia <rolando.garcia@businessol.com>
2020-02-25 00:13:05 +0000

    Debugging dc get record on server

--- dc_api/get_record/index.php
+++ dc_api/get_record/index.php
@@ -2,6 +2,7 @@
 include('../headers.php');
 include('../filebase.php');
 $uid = $_REQUEST['uid'];
+print_r($uid);
 $record = $database->get($uid);
 $array = $record->toArray();
 exit(json_encode($array));
