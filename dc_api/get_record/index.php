b646d96391585090859c9906dc30a4c233217656
Rolo Garcia <rolando.garcia@businessol.com>
2020-02-25 00:15:47 +0000

    Removing Debug

--- dc_api/get_record/index.php
+++ dc_api/get_record/index.php
@@ -2,7 +2,6 @@
 include('../headers.php');
 include('../filebase.php');
 $uid = $_REQUEST['uid'];
-print_r($uid);
 $record = $database->get($uid);
 $array = $record->toArray();
 exit(json_encode($array));
