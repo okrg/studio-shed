fdb0007f989ea4fab2b6962c8fd0ca5465a0eca6
Rolo Garcia <rolando.garcia@businessol.com>
2020-05-14 23:15:08 +0000

    Testing change to env to use $_ENV['http_host'] to get current pantheon server

--- dc_api/env.php
+++ dc_api/env.php
@@ -1,7 +1,10 @@
 <?php
-//DEV 2 ennvironment
-$host = 'https://dev2-studio-shed.pantheonsite.io';
-//Localhost
-//$host = 'http://studioshed2020:8888/';
+if(isset($_ENV['HTTP_HOST'])) {
+  $http_host = $_ENV['HTTP_HOST'];
+} else {
+  $http_host = $_SERVER['HTTP_HOST'];
+}
+
+$host = 'http://'.$http_host;
 $email_list_id = '8c1054e78f19705b462d5c5651f8dce6';
 $email_api_key = 'b2bcacdab42c5b79d24d1963c5f67e7dbdda724129014bda';
