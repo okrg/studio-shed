43aae0c9d3326259c48bc97ae3ed9da99c3549ab
Rolo Garcia <rolando.garcia@businessol.com>
2020-06-03 19:14:40 +0000

    Tweak to remove php notice from live login redirect

--- dc/includes/logged-out-header.php
+++ dc/includes/logged-out-header.php
@@ -1,5 +1,6 @@
 <?php
-
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
 session_start();
 if (empty($_SESSION['csrf_token'])) {
     $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
