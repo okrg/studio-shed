57b8020d61497ec2e3ae85381707102d14ef5a0c
Rolo Garcia <rgarcia@studioshed.com>
2021-03-23 03:20:27 +0000

    Adding sales tax message to checkout order table

--- wp-content/themes/studio-shed-child/woocommerce/checkout/review-order.php
+++ wp-content/themes/studio-shed-child/woocommerce/checkout/review-order.php
@@ -116,7 +116,9 @@ defined( 'ABSPATH' ) || exit;
 
     <tr class="order-total">
       <th><?php esc_html_e( 'Total', 'woocommerce' ); ?></th>
-      <td><?php wc_cart_totals_order_total_html(); ?></td>
+      <td><?php wc_cart_totals_order_total_html(); ?>
+        <div>Sales Tax May Apply</div>
+      </td>
     </tr>
 
     <?php do_action( 'woocommerce_review_order_after_order_total' ); ?>
