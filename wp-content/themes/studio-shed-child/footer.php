c41bf3def1d7b0602eecb62480eefe985138d04b
Rolo Garcia <rgarcia@studioshed.com>
2021-04-22 18:20:06 +0000

    Add JS to open external links in a new tab

--- wp-content/themes/studio-shed-child/footer.php
+++ wp-content/themes/studio-shed-child/footer.php
@@ -239,6 +239,20 @@
 </div> -->
 
 <script type="text/javascript">
+    //Open external links in a new window
+    jQuery(document).ready(function($) {
+        $('a').each(function() {
+        var a = new RegExp('/' + window.location.host + '/');
+        if(!a.test(this.href)) {
+            $(this).click(function(event) {
+                event.preventDefault();
+                event.stopPropagation();
+                window.open(this.href, '_blank');
+            });
+        }
+        });
+    });
+
 
     jQuery('#gform_11').submit(function () {
         var value = jQuery('.range').val();
