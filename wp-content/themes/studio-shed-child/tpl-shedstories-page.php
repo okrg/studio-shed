041993ec04022cf02f496999ebc88dedb2feaeb1
Rolo Garcia <rgarcia@studioshed.com>
2021-04-27 17:16:27 +0000

    Shed stories sticky CTA

--- wp-content/themes/studio-shed-child/tpl-shedstories-page.php
+++ wp-content/themes/studio-shed-child/tpl-shedstories-page.php
@@ -25,5 +25,31 @@
 <?php endwhile; endif; ?>
 
 <?//php get_template_part('button-create'); ?>
+<script src="https://apps.elfsight.com/p/platform.js" defer></script>
+<div class="elfsight-app-45f44944-baf8-48c1-9d65-70a6bd3fe9b1"></div>
+
+<style>
+.eapp-popup-block-variation-text-text {
+  white-space: normal;
+  text-align: center;
+  text-transform: uppercase;
+  font-weight: bold;
+  font-family: "Futura-PT-Heavy" !important;
+}
+.eapp-popup-button-component {
+  white-space: normal;
+  text-align: center;
+  line-height: 18px;
+  height: auto !important;
+  padding-top: 4px;
+  padding-bottom: 4px;
+  text-transform: uppercase;
+  font-weight: bold;
+  font-family: "Futura-PT-Book";
+  border: 1px #fff solid;
+  box-shadow: 0px 0px 10px rgba(0,0,0,0.33);
+
+}
+</style>
 
 <?php get_footer('shedstories-vc'); ?>
