f02c514f124801aafbf0871619f56d4b2d758224
Rolo Garcia <rgarcia@studioshed.com>
2021-07-15 00:17:59 +0000

    QS promo display changes for 10% off

--- wp-content/themes/studio-shed-child/inc/quick-ship-grid.php
+++ wp-content/themes/studio-shed-child/inc/quick-ship-grid.php
@@ -1,12 +1,11 @@
 <div class="container">
   <div class="qs-intro">
   <div class="product-headline text-uppercase">DIY Quick-Ship Models</div>
+  <div class="promo-headline">Additional 10% Off – 10 Days Only!</div>
   <div class="product-headline">Our Most Popular Models. Faster Turnaround.</div>
   <p>These models are the best of our Signature Series. Our most popular designs, delivered to your door in weeks. Choose the perfect size, colors, and options for your perfect home office, studio, or backyard retreat. Specially priced for DIY installation only with <u>standard shipping included</u>.</p>
 
 
-<div class="qs-disclaimer">* Everyday discount applied. Not eligible for additional promos.</div>
-<div class="qs-disclaimer">NOTE: Due to the current high demand, turnaround time is typically 6-8 weeks in most markets.</div>
   </div>
 
   <div class="inside qs-grid">
@@ -14,7 +13,9 @@
         <a href="/product/pagoda/">
             <div class="qs-name">Pagoda</div>
             <p>The Original with All-New Updates</p>
-            <div class="qs-price">$19,295*</div>
+            <div class="qs-price">$17,365*</div>
+            <div class="qs-original">$19,925</div>
+            <div class="qs-savings">Save $4,885</div>
             <img src="/wp-content/themes/studio-shed-child/img/qs-pagoda.png">
         </a>
     </article>
@@ -22,7 +23,9 @@
         <a href="/product/boreas/">
             <div class="qs-name">Boreas</div>
             <p>French Doors and a Flexible Layout</p>
-            <div class="qs-price">$19,495*</div>
+            <div class="qs-price">$17,545*</div>
+            <div class="qs-original">$19,495</div>
+            <div class="qs-savings">Save $4,740</div>
             <img src="/wp-content/themes/studio-shed-child/img/qs-boreas.png">
         </a>
     </article>
@@ -30,7 +33,9 @@
         <a href="/product/solitude/">
             <div class="qs-name">Solitude</div>
             <p>Your Turnkey Backyard Sanctuary</p>
-            <div class="qs-price">$21,195*</div>
+            <div class="qs-price">$19,075*</div>
+            <div class="qs-original">$21,195</div>
+            <div class="qs-savings">Save $5,110</div>
             <img src="/wp-content/themes/studio-shed-child/img/qs-solitude.png">
         </a>
     </article>
@@ -38,9 +43,15 @@
         <a href="/product/telluride/">
             <div class="qs-name">Telluride</div>
             <p>Light-Filled Backyard Retreat or Studio</p>
-            <div class="qs-price">$22,895*</div>
+            <div class="qs-price">$20,605*</div>
+            <div class="qs-original">$22,895</div>
+            <div class="qs-savings">Save $5,350</div>
             <img src="/wp-content/themes/studio-shed-child/img/qs-telluride.png">
         </a>
     </article>
   </div>
+
+    <div class="qs-disclaimer">* Everyday discount applied. Not eligible for additional promos.</div>
+    <div class="qs-disclaimer">NOTE: Due to the current high demand, turnaround time is typically 6-8 weeks in most markets.</div>
+
 </div>
