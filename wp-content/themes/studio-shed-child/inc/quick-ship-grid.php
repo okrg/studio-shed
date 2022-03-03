43602f3a87dc485ef1c997721345aeed1ee73a13
Rolo Garcia <rgarcia@studioshed.com>
2022-03-03 20:16:17 +0000

    spring-sale-updates

--- wp-content/themes/studio-shed-child/inc/quick-ship-grid.php
+++ wp-content/themes/studio-shed-child/inc/quick-ship-grid.php
@@ -1,18 +1,17 @@
 <div class="container">
   <div class="qs-intro">
   <div class="product-headline text-uppercase">DIY Quick-Ship Models</div>
-  <?php
+  
   /*
-  <div class="promo-headline">Additional 5% Off – Order by January 17th!</div>
+  <div class="promo-headline">Additional 5% Off – Order by March 21st!</div>
   */
-  ?>
+  
   <div class="product-headline">Our Most Popular Models. Faster Turnaround.</div>
   <p>These models are the best of our Signature Series. Our most popular designs, delivered to your door in weeks. Choose the perfect size, colors, and options for your perfect home office, studio, or backyard retreat. Specially priced for DIY installation only with <u>standard shipping included</u>.</p>
 
 
   </div>
 
-<?php /*
   <div class="inside qs-grid mt-4">
     <article>
       <header>
@@ -23,9 +22,9 @@
       </header>
       <footer>
         <a href="/product/pagoda/">
-          <div class="qs-price">$18,332*</div>
-          <div class="qs-original">$22,250</div>
-          <div class="qs-savings">Save $3,198</div>
+          <div class="qs-price">$19,409*</div>
+          <div class="qs-original">$22,206</div>
+          <div class="qs-savings">Save $2,798</div>
           <img src="/wp-content/themes/studio-shed-child/img/qs-pagoda.png">
         </a>
       </footer>
@@ -39,9 +38,9 @@
       </header>
       <footer>
         <a href="/product/boreas/">
-            <div class="qs-price">$18,520*</div>
-            <div class="qs-original">$22,285</div>
-            <div class="qs-savings">Save $3,765</div>
+            <div class="qs-price">$19,016*</div>
+            <div class="qs-original">$21,758</div>
+            <div class="qs-savings">Save $2,742</div>
             <img src="/wp-content/themes/studio-shed-child/img/qs-boreas.png">
         </a>
       </footer>
@@ -55,9 +54,9 @@
       </header>
       <footer>
         <a href="/product/solitude/">
-            <div class="qs-price">$20,135*</div>
-            <div class="qs-original">$24,185</div>
-            <div class="qs-savings">Save $4,050</div>
+            <div class="qs-price">$21,455*</div>
+            <div class="qs-original">$24,548</div>
+            <div class="qs-savings">Save $3,093</div>
             <img src="/wp-content/themes/studio-shed-child/img/qs-solitude.png">
         </a>
       </footer>
@@ -71,59 +70,15 @@
       </header>
       <footer>
         <a href="/product/telluride/">
-            <div class="qs-price">$21,750*</div>
-            <div class="qs-original">$25,955</div>
-            <div class="qs-savings">Save $4,250</div>
+            <div class="qs-price">$22,609*</div>
+            <div class="qs-original">$25,868</div>
+            <div class="qs-savings">Save $3,259</div>
             <img src="/wp-content/themes/studio-shed-child/img/qs-telluride.png">
         </a>
       </footer>
     </article>
   </div>
-  */?>
 
-  
-  <div class="inside qs-grid mt-4">
-    <article>
-        <a href="/product/pagoda/">
-            <div class="qs-name">Pagoda</div>
-            <p>The Original with All-New Updates</p>
-            <div class="qs-price">$19,295*</div>
-            <div class="qs-original">$22,250</div>
-            <div class="qs-savings">Save $2,950</div>
-            <img src="/wp-content/themes/studio-shed-child/img/qs-pagoda.png">
-        </a>
-    </article>
-    <article>
-        <a href="/product/boreas/">
-            <div class="qs-name">Boreas</div>
-            <p>French Doors and a Flexible Layout</p>
-            <div class="qs-price">$19,495*</div>
-            <div class="qs-original">$22,285</div>
-            <div class="qs-savings">Save $2,790</div>
-            <img src="/wp-content/themes/studio-shed-child/img/qs-boreas.png">
-        </a>
-    </article>
-    <article>
-        <a href="/product/solitude/">
-            <div class="qs-name">Solitude</div>
-            <p>Your Turnkey Backyard Sanctuary</p>
-            <div class="qs-price">$21,195*</div>
-            <div class="qs-original">$24,185</div>
-            <div class="qs-savings">Save $2,990</div>
-            <img src="/wp-content/themes/studio-shed-child/img/qs-solitude.png">
-        </a>
-    </article>
-    <article>
-        <a href="/product/telluride/">
-            <div class="qs-name">Telluride</div>
-            <p>Light-Filled Backyard Retreat or Studio</p>
-            <div class="qs-price">$22,895*</div>
-            <div class="qs-original">$25,955</div>
-            <div class="qs-savings">Save $3,060</div>
-            <img src="/wp-content/themes/studio-shed-child/img/qs-telluride.png">
-        </a>
-    </article>
-  </div>
 
   <div class="qs-disclaimer">* Everyday discount applied. Not eligible for additional promos.</div>
   <div class="qs-disclaimer">NOTE: Due to the current high demand, turnaround time is typically 6-8 weeks in most markets.</div>
