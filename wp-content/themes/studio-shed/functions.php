35ce4ed243206222219e27c69d0fd1913f1b94c4
Rolo Garcia <rgarcia@studioshed.com>
2022-05-09 22:29:08 +0000

    testing gforms js fix

--- wp-content/themes/studio-shed/functions.php
+++ wp-content/themes/studio-shed/functions.php
@@ -866,6 +866,8 @@ if( function_exists('acf_add_options_page') ) {
 }
 // for new homepage template
 //add_filter( 'gform_init_scripts_footer', '__return_true' );
+add_filter( 'gform_force_hooks_js_output', '__return_true' );
+
 add_filter( 'gform_cdata_open', 'wrap_gform_cdata_open', 1 );
 function wrap_gform_cdata_open( $content = '' ) {
 	if ( ( defined('DOING_AJAX') && DOING_AJAX ) || isset( $_POST['gform_ajax'] ) ) {
