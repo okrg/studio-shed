c6ba515339cd06cb39db33c504af97402b7068fa
Rolo Garcia <rgarcia@studioshed.com>
2022-07-14 01:34:40 +0000

    Removing gravity forms (finally)

--- wp-content/plugins/gravity-forms-no-captcha-recaptcha/public/partials/gf-no-captcha-recaptcha-public-theme-dropdown.php
+++
@@ -1,9 +0,0 @@
-<li class="recaptcha_theme_setting field_setting">
-
-	<label for="field_theme_setting"><?php _e( 'CAPTCHA Theme', 'gravityforms' ); ?></label>
-
-	<select id="field_recaptcha_theme_value" onchange="SetFieldProperty( 'recaptcha_theme', this.value );">
-		<option value="light">Light</option>
-		<option value="dark">Dark</option>
-	</select>
-</li>
