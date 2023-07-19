fa2395b3e439dba7a33af63d766b18c24eda900e
Rolo <rolando.garcia@gmail.com>
2023-07-18 19:23:35 -0700

    UPdating my adu roi

--- wp-content/themes/studio-shed-child/static-home.php
+++ wp-content/themes/studio-shed-child/static-home.php
@@ -25,8 +25,8 @@
   <link rel="dns-prefetch" href="//js.hsforms.net">
   <link rel="canonical" href="https://www.studio-shed.com/">
   <link rel="shortcut icon" href="/assets/images/favicon.ico">
-  <link rel="stylesheet" href="/assets/main.bundle.css?v=1689726032157">
-  <script src="/assets/main.bundle.js?v=1689726032157"></script>
+  <link rel="stylesheet" href="/assets/main.bundle.css?v=1689733384059">
+  <script src="/assets/main.bundle.js?v=1689733384059"></script>
   <script src="//js.hsforms.net/forms/v2.js"></script>
   <!--
   <script src="https://apps.elfsight.com/p/platform.js" async></script>
