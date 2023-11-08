171c7f78d0eae44d7bd3879ccf4aceb03b89593b
Rolo <rolando.garcia@gmail.com>
2023-11-08 15:12:31 -0800

    Aspect RFC page

--- wp-content/themes/studio-shed-child/static-home.php
+++ wp-content/themes/studio-shed-child/static-home.php
@@ -25,8 +25,8 @@
   <link rel="dns-prefetch" href="//js.hsforms.net">
   <link rel="canonical" href="https://www.studio-shed.com/">
   <link rel="shortcut icon" href="/assets/images/favicon.ico">
-  <link rel="stylesheet" href="/assets/main.bundle.css?v=1699484296378">
-  <script src="/assets/main.bundle.js?v=1699484296378"></script>
+  <link rel="stylesheet" href="/assets/main.bundle.css?v=1699485140326">
+  <script src="/assets/main.bundle.js?v=1699485140326"></script>
   <script src="//js.hsforms.net/forms/v2.js"></script>
   <!--
   <script src="https://apps.elfsight.com/p/platform.js" async></script>
