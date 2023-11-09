17f21a4ade0ddf2f00f62e36e74e529d9023102f
Rolo <rolando.garcia@gmail.com>
2023-11-08 17:54:33 -0800

    Aspect title tags

--- wp-content/themes/studio-shed-child/static-home.php
+++ wp-content/themes/studio-shed-child/static-home.php
@@ -25,8 +25,8 @@
   <link rel="dns-prefetch" href="//js.hsforms.net">
   <link rel="canonical" href="https://www.studio-shed.com/">
   <link rel="shortcut icon" href="/assets/images/favicon.ico">
-  <link rel="stylesheet" href="/assets/main.bundle.css?v=1699494209535">
-  <script src="/assets/main.bundle.js?v=1699494209535"></script>
+  <link rel="stylesheet" href="/assets/main.bundle.css?v=1699494859509">
+  <script src="/assets/main.bundle.js?v=1699494859509"></script>
   <script src="//js.hsforms.net/forms/v2.js"></script>
   <!--
   <script src="https://apps.elfsight.com/p/platform.js" async></script>
