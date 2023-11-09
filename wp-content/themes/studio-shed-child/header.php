1c3c804c8051ba17fab4006ad65d1cf3a067ebe9
Rolo <rolando.garcia@gmail.com>
2023-11-08 18:01:47 -0800

    nav fix

--- wp-content/themes/studio-shed-child/header.php
+++ wp-content/themes/studio-shed-child/header.php
@@ -52,8 +52,8 @@
   <link rel="preload" href="/wp-content/uploads/visualcomposer-assets/sharedLibraries/iconpicker/dist/fonts/dripicons.ttf?24252cd33f8c8234e26505c0ea3dd9c7" as="font" crossorigin>
   <link rel="preload" href="/wp-content/themes/studio-shed/fonts/icomoon.ttf?aemgks" as="font" crossorigin>
   <link rel="shortcut icon" href="/assets/images/favicon.ico">
-  <link rel="stylesheet" href="/assets/main.bundle.css?v=1699494859509">
-  <script src="/assets/main.bundle.js?v=1699494859509"></script>
+  <link rel="stylesheet" href="/assets/main.bundle.css?v=1699495297635">
+  <script src="/assets/main.bundle.js?v=1699495297635"></script>
   <script src="//js.hsforms.net/forms/v2.js"></script>
   <?php wp_head(); ?>
   <!-- Google Optimize Anti-flicker snippet (recommended)  -->
@@ -81,7 +81,7 @@
 
 <header x-cloak x-data="window.Components.navManager">
   <div class="relative bg-white z-20">
-    <div class="flex justify-between items-center max-w-7xl mx-auto px-4 pt-6 pb-2 md:justify-start md:space-x-6">
+    <div class="flex justify-between items-center max-w-7xl mx-auto px-4 pt-4 pb-2 md:justify-start md:space-x-6">
       <div class="flex justify-start flex-1">
         <a href="/">
           <span class="sr-only">Studio Shed</span>
@@ -89,7 +89,7 @@
         </a>
       </div>
       <div class="-mr-2 -my-2 lg:hidden">
-        <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false" @click.prevent="mobileMenu = true">
+        <button type="button" class="bg-white rounded-md inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false" @click.prevent="mobileMenu = true">
           <span class="sr-only">Open menu</span>          
           <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
             <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
