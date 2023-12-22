713ed8aa6d9b65a1d6c704689dfce85dc1f2028d
Rolo <rolando.garcia@gmail.com>
2023-12-22 15:28:20 -0800

    Create 404.php

---
+++ wp-content/themes/studio-shed-child/404.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+  
+  <section id="content-header" class="container">
+    <div class="row">
+      <div class="col-md-12 intro no-slide">
+        <p class="titleintro text-uppercase">Page not found!</p>
+      </div>
+    </div>
+  </section>
+<?php get_footer(); ?>
