5bfc51dd547333001fadc6e62c8f12e9c2647623
Rolo <rolando.garcia@gmail.com>
2023-12-22 19:32:34 -0800

    OG updates

--- wp-content/themes/studio-shed-child/static-home.php
+++ wp-content/themes/studio-shed-child/static-home.php
@@ -26,8 +26,8 @@
   <link rel="dns-prefetch" href="//js.hsforms.net">
   <link rel="canonical" href="https://www.studio-shed.com/">
   <link rel="shortcut icon" href="/assets/images/favicon.ico">
-  <link rel="stylesheet" href="/assets/main.bundle.css?v=1703288703584">
-  <script src="/assets/main.bundle.js?v=1703288703584"></script>
+  <link rel="stylesheet" href="/assets/main.bundle.css?v=1703302308908">
+  <script src="/assets/main.bundle.js?v=1703302308908"></script>
   <script src="//js.hsforms.net/forms/v2.js"></script>
   <!--
   <script src="https://apps.elfsight.com/p/platform.js" async></script>
