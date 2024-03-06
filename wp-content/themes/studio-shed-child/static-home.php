e516983867747453145e9fc08b5fcadb0f93538a
Rolo <rolando.garcia@gmail.com>
2024-03-05 16:24:12 -0800

    Update static-home.php

--- wp-content/themes/studio-shed-child/static-home.php
+++ wp-content/themes/studio-shed-child/static-home.php
@@ -26,8 +26,8 @@
   <link rel="dns-prefetch" href="//js.hsforms.net">
   <link rel="canonical" href="https://www.studio-shed.com/">
   <link rel="shortcut icon" href="/assets/images/favicon.ico">
-  <link rel="stylesheet" href="/assets/main.bundle.css?v=1709277403573">
-  <script src="/assets/main.bundle.js?v=1709277403573"></script>
+  <link rel="stylesheet" href="/assets/main.bundle.css?v=1709684618240">
+  <script src="/assets/main.bundle.js?v=1709684618240"></script>
   <script src="//js.hsforms.net/forms/v2.js"></script>
   <!--
   <script src="https://apps.elfsight.com/p/platform.js" async></script>
@@ -1137,13 +1137,15 @@
           <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-2xl sm:-top-80" aria-hidden="true">
             <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
           </div>
-          <div class="mx-auto max-w-3xl pt-32">
-            <div class="text-center py-8">
+          <div class="mx-auto max-w-none">
+            <div class="text-center">
               <a href="https://design.studio-shed.com" target="_blank">
-                <img style="width:auto;height: auto;position: relative;transform: translate(-50%,-25%);" src="/assets/images/home-slider/spring24-promo.jpeg" />
+                <img class="hidden sm:block" style="width:auto;height: auto;position: relative;transform: translate(-50%,0%);" src="/assets/images/home-slider/spring24-promo.jpeg" />
+                <img class="block sm:hidden" style="width:auto;height: auto;position: relative;transform: translate(-50%,25%);" src="/assets/images/home-slider/spring24-promo.jpeg" />
               </a>
             </div>
           </div>
+
         </div>
 
 
