68f0ed24872309c49b457fedeabc3ee314362eb2
Rolo <rolando.garcia@gmail.com>
2024-05-17 14:24:37 -0700

    Summit nx launch :)

--- wp-content/themes/studio-shed-child/header.php
+++ wp-content/themes/studio-shed-child/header.php
@@ -52,8 +52,8 @@
   <link rel="preload" href="/wp-content/uploads/visualcomposer-assets/sharedLibraries/iconpicker/dist/fonts/dripicons.ttf?24252cd33f8c8234e26505c0ea3dd9c7" as="font" crossorigin>
   <link rel="preload" href="/wp-content/themes/studio-shed/fonts/icomoon.ttf?aemgks" as="font" crossorigin>
   <link rel="shortcut icon" href="/assets/images/favicon.ico">
-  <link rel="stylesheet" href="/assets/main.bundle.css?v=1714060130846">
-  <script src="/assets/main.bundle.js?v=1714060130846"></script>
+  <link rel="stylesheet" href="/assets/main.bundle.css?v=1715981018824">
+  <script src="/assets/main.bundle.js?v=1715981018824"></script>
   <script src="//js.hsforms.net/forms/v2.js"></script>
   <?php wp_head(); ?>
   <!-- Google Optimize Anti-flicker snippet (recommended)  -->
