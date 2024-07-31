a5769038175602e1b44a04a6a25aadf5d202a1d3
Rolo <rolando.garcia@gmail.com>
2024-07-30 17:27:17 -0700

    Update static-home.php

--- wp-content/themes/studio-shed-child/static-home.php
+++ wp-content/themes/studio-shed-child/static-home.php
@@ -26,8 +26,8 @@
   <link rel="dns-prefetch" href="//js.hsforms.net">
   <link rel="canonical" href="https://www.studio-shed.com/">
   <link rel="shortcut icon" href="/assets/images/favicon.ico">
-  <link rel="stylesheet" href="/assets/main.bundle.css?v=1722384875190">
-  <script src="/assets/main.bundle.js?v=1722384875190"></script>
+  <link rel="stylesheet" href="/assets/main.bundle.css?v=1722385627276">
+  <script src="/assets/main.bundle.js?v=1722385627276"></script>
   <script src="//js.hsforms.net/forms/v2.js"></script>
   <!--
   <script src="https://apps.elfsight.com/p/platform.js" async></script>
@@ -1281,9 +1281,6 @@
             <source srcset="/assets/images/home-slider/summer-15-hero.webp" type="image/webp">
             <img src="/assets/images/home-slider/summer-15-hero.jpg" alt="" class="absolute inset-0 h-full w-full object-cover" loading="lazy" style="z-index:-10">
           </picture>
-          <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
-            <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
-          </div>
           <div class="mx-auto max-w-3xl py-12 sm:py-48 lg:py-48">
             <div class="text-center py-8" style="background:rgba(0,0,0,0.33)">
               <h2 class="text-2xl font-bold tracking-tight text-white sm:text-4xl drop-shadow-2xl">Faster than Design-Build</h2>
@@ -1302,9 +1299,6 @@
             <source srcset="/assets/images/home-slider/summer-15-02.webp" type="image/webp">
             <img src="/assets/images/home-slider/summer-15-02.jpg" alt="A Studio Shed building assembled in a back yard" loading="lazy" class="absolute inset-0 h-full w-full object-cover" style="z-index:-10">
           </picture>
-          <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
-            <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
-          </div>
           <div class="mx-auto max-w-3xl py-12 sm:py-48 lg:py-48">
             <div class="text-center py-8" style="background:rgba(0,0,0,0.33)">
               <h2 class="text-2xl font-bold tracking-tight text-white sm:text-4xl drop-shadow-2xl">High-Quality Nimble Building Kit</h2>
@@ -1323,9 +1317,6 @@
             <source srcset="/assets/images/home-slider/03.webp" type="image/webp">
             <img src="/assets/images/home-slider/03.jpg" alt="" class="absolute inset-0 h-full w-full object-cover" loading="lazy" style="z-index:-10">
           </picture>
-          <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
-            <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
-          </div>
           <div class="mx-auto max-w-3xl py-12 sm:py-48 lg:py-48">
             <div class="text-center py-8" style="background:rgba(0,0,0,0.33)">
               <h2 class="text-2xl font-bold tracking-tight text-white sm:text-4xl drop-shadow-2xl">Value</h2>
